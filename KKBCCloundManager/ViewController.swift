0b395160283493beef163b6680f5dcf444d50579
ThornDev <jiemianwang@gmail.com>
2017-01-13 12:33:03 +0800

    Initial Commit

---
+++ KKBCCloundManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KKBCCloundManager
+//
+//  Created by 王绵杰 on 2017/1/13.
+//  Copyright © 2017年 王绵杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
